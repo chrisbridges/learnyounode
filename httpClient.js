6de191556588e9092392acac065965f71122dd97
chris.bridges <christopher_bridges@icloud.com>
2018-08-06 18:15:04 -0400

    finished httpclient

--- httpClient.js
+++ httpClient.js
@@ -1,2 +1,19 @@
 const http = require('http');
 
+const urlToFetch = process.argv[2];
+
+http.get(urlToFetch, function (response) {
+  response.on('data', function (data) {
+    console.log(data);
+  }).setEncoding('utf8');
+});
+
+// official solution
+
+// var http = require('http')
+    
+// http.get(process.argv[2], function (response) {
+//   response.setEncoding('utf8')
+//   response.on('data', console.log)
+//   response.on('error', console.error)
+// }).on('error', console.error)
