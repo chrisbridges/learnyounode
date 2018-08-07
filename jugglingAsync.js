489742765df684a538e650886c0c020d8b6aa2dc
chris.bridges <christopher_bridges@icloud.com>
2018-08-06 20:32:28 -0400

    cleanup and original solution attached

--- jugglingAsync.js
+++ jugglingAsync.js
@@ -12,15 +12,6 @@ for (let i = 0; i < 3; i++) {
 urls.forEach((url, index) => {
   // use a pipe to ensure you get all the data per url
   http.get(url, function (response) {
-    response.setEncoding('utf8');
-    // response.on('data', function (data) {
-    //   results[index] = data.toString();
-    //   resultsCount++;
-
-    //   if (!results.includes(null)) {
-    //     results.forEach(result => console.log(result));
-    //   }
-    // });
     response.pipe(bl((err, data) => {
       results[index] = data.toString();
       resultsCount++;
@@ -29,10 +20,38 @@ urls.forEach((url, index) => {
         results.forEach(result => console.log(result));
       }
     }));
-    // response.on('end', function () {
-    //   if (!results.includes(null)) {
-    //     results.forEach(result => console.log(result));
-    //   }
-    // });
   });
 });
+
+// provided solution
+// var http = require('http')
+//     var bl = require('bl')
+//     var results = []
+//     var count = 0
+    
+//     function printResults () {
+//       for (var i = 0; i < 3; i++) {
+//         console.log(results[i])
+//       }
+//     }
+    
+//     function httpGet (index) {
+//       http.get(process.argv[2 + index], function (response) {
+//         response.pipe(bl(function (err, data) {
+//           if (err) {
+//             return console.error(err)
+//           }
+    
+//           results[index] = data.toString()
+//           count++
+    
+//           if (count === 3) {
+//             printResults()
+//           }
+//         }))
+//       })
+//     }
+    
+//     for (var i = 0; i < 3; i++) {
+//       httpGet(i)
+//     }
