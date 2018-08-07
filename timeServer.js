8ce628438ce577b228a3f756c0fe70c9bf173c67
chris.bridges <christopher_bridges@icloud.com>
2018-08-07 10:46:24 -0400

    timeServer complete

--- timeServer.js
+++ timeServer.js
@@ -7,6 +7,16 @@ const server = net.createServer(socket => {
   // socket.end(data) sends data before closing
   // so since we're only writing one thing to the server
   // we can just use that instead of socket.write(data)
-  
+  socket.end(getCurrentDateAndTime());
 });
 server.listen(port);
+
+function getCurrentDateAndTime () {
+  const today = new Date();
+
+  return `${today.getFullYear()}-${zeroPadding(today.getMonth() + 1)}-${zeroPadding(today.getDate())} ${zeroPadding(today.getHours())}:${zeroPadding(today.getMinutes())}\n`;
+}
+
+function zeroPadding (num) {
+  return num < 10 ? `0${num}` : num;
+}
